4556b2c5cd7b9d3229de8edf910031c4e4617e6f
Sumitholkar143 <137525163+Sumitholkar143@users.noreply.github.com>
2023-07-07 19:48:18 +0530

    Update Params.java

--- app/src/main/java/com/example/onlineexam/params/Params.java
+++ app/src/main/java/com/example/onlineexam/params/Params.java
@@ -4,11 +4,11 @@ package com.example.onlineexam.params;
 
 public class Params {
     public static final String url="jdbc:mysql://localhost:3306/name";
-    public static final String uname="sumitdb";
-    public static final String pass="root2";
+    public static final String uname=username";
+    public static final String pass="password";
     public static final String Stu_DB_NAME="studentdetails";
     public static final String DRIVER_CLASS="net.sourceforge.jtds.jdbc.Driver";
-    public static final String CONNECTION_STRING="jdbc:jtds:sqlserver://192.168.43.130"+":1433/master";
+    public static final String CONNECTION_STRING="url";
     public static final String GKQPDB="GKQPDB";
     public static final String MAQPDB="MAQPDB";
     public static final String MAQPDT="MAQPDT";
